327165841ff3a63ae035eaf68e63b8dc8f0a519b
Andrei Manea <gabrielandrei12@gmail.com>
2020-10-20 19:10:28 +0300

    rezolvare tema 4

---
+++ Manea Andrei-Gabriel/L04/Question.Domain/PostQuestionWorkflow/PostQuestionCmd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Question.Domain.PostQuestionWorkflow
+{
+    // product type = Title*Body*Tags
+    public struct PostQuestionCmd
+    {
+        [MaxLength(150)]
+        [Required]
+        public string Title { get; private set; } // question title
+        [Required]
+        public string Body { get; set; } // question body ( question description )
+        [Required]
+        public List<string> Tags { get; set; } // question tags . Am folosit lista pentru ca putem adauga mai multe tags.
+        
+        public PostQuestionCmd(string title, string body, List<string> tags)
+        {
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+    }
+}
---
+++ Manea Andrei-Gabriel/L04/Test.App/ProgramQuestion.cs
@@ -0,0 +1,75 @@
+﻿using Question.Domain.PostQuestionWorkflow;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using static Question.Domain.PostQuestionWorkflow.PostQuestionResult;
+
+namespace Test.App
+{
+    class ProgramQuestion
+    {
+        static void Main2(string[] args)
+        {
+            List<string> tags = new List<string> {"React", "JavaScript"};
+            var cmd = new PostQuestionCmd("What are Components?", "Can somebody explain me how Components work in React", tags);
+            var result = PostQuestion(cmd);
+
+            result.Match(
+                    ProcessQuestionPosted,
+                    ProcessQuestionNotPosted,
+                    ProcessInvalidQuestion
+                );
+
+            Console.ReadLine();
+        }
+
+        private static IPostQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+
+        private static IPostQuestionResult ProcessQuestionNotPosted(QuestionNotPosted questionNotCreatedResult)
+        {
+            Console.WriteLine($"Question not created: {questionNotCreatedResult.Reason}");
+            return questionNotCreatedResult;
+        }
+
+        private static IPostQuestionResult ProcessQuestionPosted(QuestionPosted question)
+        {
+            Console.WriteLine($"Question {question.QuestionId}");
+            return question;
+        }
+
+        public static IPostQuestionResult PostQuestion(PostQuestionCmd postQuestionCommand)
+        {
+            if (string.IsNullOrWhiteSpace(postQuestionCommand.Title))
+            {
+                var errors = new List<string>() { "Invalid question title" };
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (string.IsNullOrWhiteSpace(postQuestionCommand.Body))
+            {
+                var errors = new List<string>() { "Invalid body for the question" };
+                return new QuestionValidationFailed(errors);
+            }
+      
+
+            if (new Random().Next(10) > 1)
+            {
+                return new QuestionNotPosted("Question could not be verified");
+            }
+
+            var questionId = Guid.NewGuid();
+            var result = new QuestionPosted(questionId, postQuestionCommand.Title, postQuestionCommand.Body, postQuestionCommand.Tags);
+
+            //execute logic
+            return result;
+        }
+    }
+}
