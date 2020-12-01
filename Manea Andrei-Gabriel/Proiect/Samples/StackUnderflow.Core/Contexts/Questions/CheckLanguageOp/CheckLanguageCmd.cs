b12759cd6195f9fe4e3bda2da1220adf9c6572ec
Andrei Manea <gabrielandrei12@gmail.com>
2020-12-01 15:57:06 +0200

    Proiect PSSC

---
+++ Manea Andrei-Gabriel/Proiect/Samples/StackUnderflow.Core/Contexts/Questions/CheckLanguageOp/CheckLanguageCmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackUnderflow.Domain.Schema.Questions.CheckLanguageOp
+{
+    public class CheckLanguageCmd
+    {
+        public CheckLanguageCmd()
+        {
+
+        }
+
+        public CheckLanguageCmd(string text)
+        {
+            Text = text;
+        }
+
+        [Required]
+        public string Text { get; }
+    }
+}
---
+++ Manea Andrei-Gabriel/Proiect/Samples/StackUnderflow.Core/Contexts/Questions/PostQuestionOp/PostQuestionCmd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StackUnderflow.Domain.Core.Contexts.Questions.PostQuestionOp
+{
+    public class PostQuestionCmd
+    {
+        public PostQuestionCmd()
+        {
+
+        }
+
+        public PostQuestionCmd(string title, string body, List<string> tags, int votes)
+        {
+            Title = title;
+            Body = body;
+            Tags = tags;
+            Votes = votes;
+        }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(1000)]
+        public string Body { get; set; }
+        [Required]
+        public List<string> Tags { get; set; }
+        [Required]
+        public int Votes { get; set; }
+    }
+}
